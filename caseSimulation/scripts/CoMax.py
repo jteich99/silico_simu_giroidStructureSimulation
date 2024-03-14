7dfc751a5a2c8355567b9b6dc9ec8f1d740facf1
jteich99 <jteich@fi.uba.ar>
2024-03-14 12:01:29 -0300

    make deltaTime from Courant analysis be multiple of 0.1, 0.25 or 0.5

--- caseSimulation/scripts/CoMax.py
+++ caseSimulation/scripts/CoMax.py
@@ -17,9 +17,34 @@ with open('Co.sh', 'w') as outputCoFile, open(f'{latestTime}/Co', 'r') as source
             except:
                 pass
     # print(f'deltaTime for Co = 1 = {1/maxCo}') 
+    deltaTime = 0.8 / maxCo
+    if deltaTime < 0.01 and deltaTime > 0.005:
+        deltaTime = 0.005
+    elif deltaTime < 0.025 and deltaTime > 0.01:
+        deltaTime = 0.01
+    elif deltaTime < 0.05 and deltaTime > 0.025:
+        deltaTime = 0.025
+    elif deltaTime < 0.1 and deltaTime > 0.05:
+        deltaTime = 0.05
+    elif deltaTime < 0.25 and deltaTime > 0.1:
+        deltaTime = 0.1
+    elif deltaTime < 0.5 and deltaTime > 0.25:
+        deltaTime = 0.25
+    elif deltaTime < 1 and deltaTime > 0.5:
+        deltaTime = 0.5
+    elif deltaTime < 2.5 and deltaTime > 1:
+        deltaTime = 1
+    elif deltaTime < 5 and deltaTime > 2.5:
+        deltaTime = 2.5
+    elif deltaTime < 10 and deltaTime > 5:
+        deltaTime = 5
+    elif deltaTime > 10:
+        deltaTime = 10
+
+
     outputCoFile.writelines(
         f'maxCo={maxCo}\n' +
-        f'deltaTime={ 0.8 / maxCo}'
+        f'deltaTime={ deltaTime }'
     )
         # finally:
         #     pass
